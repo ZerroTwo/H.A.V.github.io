c2730150a8296e8a980c62ff4cd83cf829c52b81
Alexay <79530265+ZerroTwo@users.noreply.github.com>
2021-03-01 22:33:44 +0300

    Add files via upload

---
+++ Project/js/script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+	let btnLock = document.getElementById('btn-lock');
+	let a = document.querySelectorAll('.a');
+	let scene = document.getElementById('scene');
+	let parallaxInstance  = new Parallax(scene);
+function lock() {
+	var win = $(window).height() + 1;
+	$('html, body').animate({scrollTop: win}, 800);
+	return false;
+};
+btnLock.addEventListener('click' , lock);
+function blockButton(){
+	let i = 0;
+	return function e (){
+		if( i == a.length - 1){
+			a[i].addEventListener('click' , function(){
+				alert('In Developing');
+			})
+		} else {
+			i++
+			e();
+		};};};
+const blockactive = blockButton();
+blockactive();
+}

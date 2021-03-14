c1f3f9abffddfd4147de1b6b9efeb516b65b92b8
Alexay <79530265+ZerroTwo@users.noreply.github.com>
2021-03-14 13:07:28 +0300

    Add files via upload

---
+++ Project/js/EmailForm.js
@@ -0,0 +1,63 @@
+$('#sendMail').on('click' , function(){
+	var email = $('#email').val().trim();
+	var name = $('#name').val().trim();
+	var message = $('#message').val();
+
+/*
+	const featureCss = document.querySelector('.feature-css');
+	const feature = 'feature';
+	function add(){
+		featureCss.classList.add('active');
+	};
+	function remove(){
+		featureCss.classList.remove('active');
+	};
+
+	if(feature === email){
+		add();
+		setTimeout(remove , 3000);
+		return false;
+	} else if (feature === name){
+		add();
+		setTimeout(remove , 3000);
+		return false;
+	} else {
+		console.log('Ты был близок в фиче');
+	};*/
+	if (name == '') {
+		$('.btn-name , .btn-email , .type-order').removeClass('error');
+		$('#errorMessageName , #errorMessageEmail , #errorMessageText').html('');
+		$('#errorMessageName').text('Введите Имя');
+		$('.btn-name').addClass('error');
+		return false;
+	} else if (email == ''){
+		$('.btn-name , .btn-email , .type-order').removeClass('error');
+		$('#errorMessageName , #errorMessageEmail , #errorMessageText').html('');
+		$('#errorMessageEmail').text('Введите email');
+		$('.btn-email').addClass('error');
+		return false;
+	}	else if (message.length < 10) {
+		$('.btn-name , .btn-email , .type-order').removeClass('error');
+		$('#errorMessageName , #errorMessageEmail , #errorMessageText').html('');
+		$('#errorMessageText').text('Введите не менее 10 символов');
+		$('.type-order').addClass('error');
+		return false;
+	}
+	$('#errorMessage').text('');
+	$.ajax({
+		url: 'ajax/mail.php',
+		type: 'POST',
+		cache : false,
+		data: {'name' : name , 'email' : email , 'message' : message},
+		dataType: 'html',
+		beforeSend: function() {
+			$('#sendMail').prop('disabled',  true);
+		},
+		success: function(data){
+			if (!data == false) 
+			alert('Были ошибки, сообщение не отправлено');
+		 else 
+		 	$('#form').trigger('reset');
+
+			$('#sendMail').prop('disabled',  false);
+}});});
